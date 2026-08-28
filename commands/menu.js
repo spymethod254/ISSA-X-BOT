8699a8d754dac1379b87cc1d2f8633963ab1bc72
spymethod <spymethod254@gmail.com>
2026-08-29 01:36:19 +0300

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -8,7 +8,7 @@ export default {
         const jid = m.key.remoteJid
         const pushName = m.pushName || 'User'
         const prefix = config.prefix
-        const totalCmds = 32
+        const totalCmds = 62
         const uptime = Math.floor(process.uptime() / 60)
         const imageUrl = 'https://files.catbox.moe/o6jrdp.jpg'
 
@@ -16,16 +16,37 @@ export default {
 
         const menuText = `
 ╭───「 *${config.botName.toUpperCase()}* 」───
-│➥  👋 Hello @${pushName}
-│➥  🤖 Bot: ${config.botName}
-│➥  👑 Owner: ${config.ownerName}
-│➥  🔣 Prefix: ${prefix}
-│➥  ⏱️ Uptime: ${uptime}m
-│➥  📦 Commands: ${totalCmds}
+│✷  👋 ʜᴇʟʟᴏ @${pushName}
+│✷  🤖 ʙᴏᴛ: ${config.botName}
+│✷  👑 ᴏᴡɴᴇʀ: ${config.ownerName}
+│✷  🔣 ᴘʀᴇꜰɪx: ${prefix}
+│✷  ⏱️ ᴜᴘᴛɪᴍᴇ: ${uptime}m
+│✷  📦 ᴄᴏᴍᴍᴀɴᴅꜱ: ${totalCmds}
 ╰─────────────────────
 
 
-╭─── ⌈ *ɢᴇɴᴇʀᴀʟ* ⌋ ╾────
+╭───── ⌈ *ɢᴇɴᴇʀᴀʟ* ⌋ ╾────
+│➤ ${prefix}fetch
+│➤ ${prefix}clearsession
+│➤ ${prefix}shell
+│➤ ${prefix}chunk
+│➤ ${prefix}save
+│➤ ${prefix}vv2
+│➤ ${prefix}profile
+│➤ ${prefix}fullpp
+│➤ ${prefix}phone
+│➤ ${prefix}jid
+│➤ ${prefix}mygroups
+│➤ ${prefix}setsudo
+│➤ ${prefix}delsudo
+│➤ ${prefix}issudo
+│➤ ${prefix}getsudo
+│➤ ${prefix}deploy
+│➤ ${prefix}subbots
+│➤ ${prefix}stopbot
+│➤ ${prefix}test2
+│➤ ${prefix}post
+│➤ ${prefix}jidcount
 │➤ ${prefix}gpp
 │➤ ${prefix}avo
 │➤ ${prefix}viewonce
@@ -35,11 +56,29 @@ export default {
 │➤ ${prefix}toviewonce
 │➤ ${prefix}toview
 │➤ ${prefix}vv
-╰─────────────────
+╰──────────────────
 
 
 ╭───「 *ᴀɪ & ᴄʜᴀᴛ* 」╾───
 │➤ ${prefix}ai <question>
+│➤ ${prefix}sora
+│➤ ${prefix}flux
+│➤ ${prefix}speechwriter
+│➤ ${prefix}muslimai
+│➤ ${prefix}wormgpt
+│➤ ${prefix}bibleai
+│➤ ${prefix}removebg
+│➤ ${prefix}vision
+│➤ ${prefix}transcribe
+│➤ ${prefix}shazam
+│➤ ${prefix}bwmai
+│➤ ${prefix}gpt4o
+│➤ ${prefix}letmegpt
+│➤ ${prefix}pollinations
+│➤ ${prefix}transcript
+│➤ ${prefix}gpthistory
+│➤ ${prefix}lastchat
+│➤ ${prefix}clearai
 │➤ ${prefix}gpt <text>
 │➤ ${prefix}imagine <prompt>
 ╰───────────────────
@@ -55,8 +94,89 @@ export default {
 │➤ ${prefix}link / revoke
 │➤ ${prefix}antilink on/off
 │➤ ${prefix}welcome on/off
+╰────────────────────
+
+╭────╼ ⌈ *ꜱᴇᴛᴛɪɴɢꜱ* ⌋ ╾───
+│➤ ${prefix}hideviewchannel
+│➤ ${prefix}anticall
+│➤ ${prefix}events
+│➤ ${prefix}settings
+│➤ ${prefix}syncsettings
+│➤ ${prefix}devicemode
+│➤ ${prefix}botname
+│➤ ${prefix}author
+│➤ ${prefix}packname
+│➤ ${prefix}timezone
+│➤ ${prefix}botpic
+│➤ ${prefix}menustyle
+│➤ ${prefix}boturl
+│➤ ${prefix}mode
+│➤ ${prefix}prefix
+│➤ ${prefix}presence
+│➤ ${prefix}greet
+│➤ ${prefix}chatbot
+│➤ ${prefix}autobio
+│➤ ${prefix}antistatusmention
+│➤ ${prefix}antilink
+│➤ ${prefix}antidelete
+│➤ ${prefix}antiedit
+│➤ ${prefix}viewonce
+│➤ ${prefix}statusantidelete
+│➤ ${prefix}allvar
+│➤ ${prefix}getvar
+│➤ ${prefix}setvar
+│➤ ${prefix}systeminfo
+╰─────────────────
+
+╭───╼ ⌈ *ꜱᴘᴏʀᴛꜱ* ⌋ ╾────
+│➤ ${prefix}livesports
+│➤ ${prefix}sportscats
+│➤ ${prefix}flive
+│➤ ${prefix}flive2
+│➤ ${prefix}predictions
+│➤ ${prefix}fstream
+│➤ ${prefix}fnews
+│➤ ${prefix}blive
+│➤ ${prefix}livescore
+│➤ ${prefix}sportnews
+│➤ ${prefix}topscorers
+│➤ ${prefix}standings
+│➤ ${prefix}fixtures
+│➤ ${prefix}gamehistory
+│➤ ${prefix}stadium
+│➤ ${prefix}team
+│➤ ${prefix}player
 ╰───────────────────
 
+╭───╼ ⌈*ɢᴇɴᴇʀᴀʟ2* ⌋ ╾───
+│➤ ${prefix}toviewonce
+│➤ ${prefix}getdesc
+│➤ ${prefix}getcategory
+│➤ ${prefix}getalias
+│➤ ${prefix}help
+│➤ ${prefix}list
+│➤ ${prefix}menu
+│➤ ${prefix}bwmgift
+│➤ ${prefix}pair
+│➤ ${prefix}location
+│➤ ${prefix}copy
+│➤ ${prefix}repo
+╰──────────────────
+
+╭────╼   ⌈ *ᴀɴɪᴍᴇ* ⌋ ╾────
+│➤ ${prefix}neko
+│➤ ${prefix}waifu
+│➤ ${prefix}konachan
+│➤ ${prefix}foxgirl
+│➤ ${prefix}animerandom
+│➤ ${prefix}animequote
+│➤ ${prefix}animeloli
+│➤ ${prefix}animemilf
+│➤ ${prefix}hwaifu
+│➤ ${prefix}hneko
+│➤ ${prefix}megumin
+│➤ ${prefix}awoo
+╰───────────────────
 
 ╭───╾「 *ᴅᴏᴡɴʟᴏᴀᴅᴇʀ* 」╼──
 │➤ ${prefix}play <song>
@@ -65,7 +185,7 @@ export default {
 │➤ ${prefix}tiktok <link>
 │➤ ${prefix}fb <link>
 │➤ ${prefix}ig <link>
-╰────────────────────
+╰──────────────────────
 
 
 ╭───╾「 *ᴛᴏᴏʟꜱ* 」╼────
@@ -84,7 +204,7 @@ export default {
 │➤ ${prefix}alive
 │➤ ${prefix}owner
 │➤ ${prefix}calc <expr>
-╰──────────────────╯
+╰───────────────────
 
 > *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɪꜱꜱᴀ x ᴜʟᴛʀᴀ*
 `
